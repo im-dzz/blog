1fa4f089aadd0948330f70531592e6f0592f7931
im-dzz <imdzz@qq.com>
2019-08-10 18:35:42 +0800

    添加日志组件

--- src/main/java/com/imdzz/blog/controller/BlogController.java
+++ src/main/java/com/imdzz/blog/controller/BlogController.java
@@ -1,5 +1,7 @@
 package com.imdzz.blog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,11 @@ public class BlogController {
 	@Autowired
 	BlogService blogService;
 	
+	Logger logger = LoggerFactory.getLogger(BlogController.class);
+	
 	@GetMapping("{serialno}")
 	public ModelAndView findBlog(@PathVariable("serialno") int id, Model model) {
+		logger.info("blogs/{serialno}:{}", id);
 		model.addAttribute("blog", blogService.findBlog(id, "").get(0));
 		return new ModelAndView("blog.html", "Blog", model);
 	}
--- src/main/java/com/imdzz/blog/controller/HomeController.java
+++ src/main/java/com/imdzz/blog/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.imdzz.blog.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +21,14 @@ public class HomeController {
 	@Autowired
 	BlogService blogService;
 	
+	private Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@GetMapping()
 	public ModelAndView home(Model model) {
+		logger.info("home接口：");
 		List<Blog> blogs = blogService.findAllBlogs();
 		model.addAttribute("blogs", blogs);
-		System.out.println("列表大小是:" + blogs.size());
-		Blog blog = blogs.get(0);
-		System.out.println(blog.getTitle());
+		logger.info("列表大小是:" + blogs.size());
 		return new ModelAndView("index.html", "Blog", model);
 	}
 	
