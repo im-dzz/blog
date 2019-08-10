63c6e3b3dbf1947b7adf8972d9a7f35037723142
im-dzz <imdzz@qq.com>
2019-08-10 18:05:53 +0800

    使用lombok

--- src/main/java/com/imdzz/blog/model/Blog.java
+++ src/main/java/com/imdzz/blog/model/Blog.java
@@ -4,10 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
+@Data
 public class Blog {
 	@Id
 	int serialno; 
@@ -17,49 +16,5 @@ public class Blog {
 	String content;
 	String createDate;
 	String updateDate;
-	
-	public int getSerialno() {
-		return serialno;
-	}
-	public void setSerialno(int serialno) {
-		this.serialno = serialno;
-	}
-	public String getTitleid() {
-		return titleid;
-	}
-	public void setTitleid(String titleid) {
-		this.titleid = titleid;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getSubTitle() {
-		return subTitle;
-	}
-	public void setSubTitle(String subTitle) {
-		this.subTitle = subTitle;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-	public String getUpdateDate() {
-		return updateDate;
-	}
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
-	
 	
 }
