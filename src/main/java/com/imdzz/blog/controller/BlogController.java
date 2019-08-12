04a691cc0078c17c0b56ed72e1393289d11c3092
im-dzz <imdzz@qq.com>
2019-08-12 20:34:24 +0800

    CONTROLLER合并

--- src/main/java/com/imdzz/blog/controller/BlogController.java
+++ src/main/java/com/imdzz/blog/controller/BlogController.java
@@ -1,5 +1,7 @@
 package com.imdzz.blog.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.imdzz.blog.model.Blog;
 import com.imdzz.blog.service.BlogService;
 
 @RestController
@@ -19,6 +22,15 @@ public class BlogController {
 	BlogService blogService;
 	
 	Logger logger = LoggerFactory.getLogger(BlogController.class);
+	
+	@GetMapping()
+	public ModelAndView home(Model model) {
+		logger.info("blogs接口：");
+		List<Blog> blogs = blogService.findAllBlogs();
+		model.addAttribute("blogs", blogs);
+		logger.info("列表大小是:" + blogs.size());
+		return new ModelAndView("index.html", "Blog", model);
+	}
 	
 	@GetMapping("{serialno}")
 	public ModelAndView findBlog(@PathVariable("serialno") int id, Model model) {
